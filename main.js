183b44a6a99016e7713b9fe55b9ca60cca551801
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-01-10 18:05:47 +0200

    added animation for header

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ menuBtn.addEventListener('click', () => {
   const isOpen = navLinks.classList.contains('open');
   menuBtnIcon.setAttribute('class', isOpen ? 'ri-close-line' : 'ri-menu-line');
 });
+
+navLinks.addEventListener('click', () => {
+  navLinks.classList.remove('open');
+  menuBtnIcon.setAttribute('class', 'ri-menu-line');
+});
+
+const scrollRevealOption = {
+  distance: '50px',
+  origin: 'bottom',
+  duration: 1000,
+};
+
+/////////////////// SCROLL ANIMATION
+
+// Header container
+ScrollReveal().reveal('.header__container p', { ...scrollRevealOption });
+
+ScrollReveal().reveal('.header__container h1', {
+  ...scrollRevealOption,
+  delay: 500,
+});
